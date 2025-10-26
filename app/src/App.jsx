2e829baa5af8d840a85ca628fac9d7157a1501f9
Maury SO <lynuxazura@gmail.com>
2025-10-26 13:46:40 -0600

    Fixed

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -44,7 +44,7 @@ function App() {
 
     try {
       setLoading(true)
-      const resp = await axios.post('/api/load', formData, {
+      const resp = await axios.post('', formData, {
         headers: { 
           'Content-Type': 'multipart/form-data',
           'Accept': 'application/json',
